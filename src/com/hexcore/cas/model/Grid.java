6ed2237d0543317de722a1ba5d9d377fa7de0b09
Megan Duncan <metalpaw@mweb.co.za>
2011-07-07 09:39:57 +0200

    Implemented Grid; Added copy constructor to Vector2i
    Change-Id: I85699609cb6ee080f65cc9e5ba868bbd47fc37d8

--- src/com/hexcore/cas/model/Grid.java
+++ src/com/hexcore/cas/model/Grid.java
@@ -4,5 +4,34 @@ import com.hexcore.cas.math.Vector2i;
 
 public abstract class Grid
 {
+	private Cell[][] cells;
+	private Vector2i size;
+	
+	public Grid(Vector2i size)
+	{
+		this.size = new Vector2i(size);
+		this.cells = new Cell[size.get(0)][size.get(1)];
+	}
+	
 	public abstract Cell[] getNeighbours(Vector2i pos);
+	
+	public Cell getCell(Vector2i pos)
+	{
+		return cells[pos.get(0)][pos.get(1)];
+	}
+	
+	public int getWidth()
+	{
+		return size.get(1);
+	}
+	
+	public int getHeight()
+	{
+		return size.get(0);
+	}
+	
+	public Vector2i getSize()
+	{
+		return size;
+	}
 }
