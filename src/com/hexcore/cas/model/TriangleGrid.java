64a39f039bcc76314c5ccc3030d7856c0552ca27
Megan Duncan <metalpaw@mweb.co.za>
2011-07-07 12:14:45 +0200

    TriangleGrid: Fixed logical errors TestTriangleGrid: Implementation complete
    Change-Id: I67ba977b57ac4c7cc9d1afad45fe4e68ed9059d7

--- src/com/hexcore/cas/model/TriangleGrid.java
+++ src/com/hexcore/cas/model/TriangleGrid.java
@@ -17,28 +17,30 @@ public class TriangleGrid extends Grid
 	private Cell[] getNeighbours(boolean up, Vector2i pos)
 	{
 		Cell[] n = new Cell[12];
+		int xSize = this.getHeight();
+		int ySize = this.getWidth();
 		int x = pos.get(0);
 		int y = pos.get(1);
 		
-		int xMIN1 = pos.get(0) - 1;
-		int xPLUS1 = pos.get(0) + 1;
-		if(xPLUS1 >= x)
-			xPLUS1 -= x;
+		int xMIN1 = x - 1;
+		int xPLUS1 = x + 1;
+		if(xPLUS1 >= xSize)
+			xPLUS1 -= xSize;
 		if(xMIN1 < 0)
-			xMIN1 += x;
+			xMIN1 += xSize;
 		
-		int yMIN2 = pos.get(1) - 2;
-		int yMIN1 = pos.get(1) - 1;
-		int yPLUS1 = pos.get(1) + 1;
-		int yPLUS2 = pos.get(1) + 2;
+		int yMIN2 = y - 2;
+		int yMIN1 = y - 1;
+		int yPLUS1 = y + 1;
+		int yPLUS2 = y + 2;
 		if(yMIN2 < 0)
-			yMIN2 += y;
+			yMIN2 += ySize;
 		if(yMIN1 < 0)
-			yMIN1 += y;
-		if(yPLUS1 >= y)
-			yPLUS1 -= y;
-		if(yPLUS2 >= y)
-			yPLUS2 -= y;
+			yMIN1 += ySize;
+		if(yPLUS1 >= ySize)
+			yPLUS1 -= ySize;
+		if(yPLUS2 >= ySize)
+			yPLUS2 -= ySize;
 		if(up)
 		{
 			n[0] = this.getCell(new Vector2i(xMIN1, yMIN1));
@@ -56,18 +58,18 @@ public class TriangleGrid extends Grid
 		}
 		else
 		{
-			n[0] = this.getCell(new Vector2i(xPLUS1, yMIN2));
-			n[1] = this.getCell(new Vector2i(xPLUS1, yMIN1));
-			n[2] = this.getCell(new Vector2i(xPLUS1, y));
-			n[3] = this.getCell(new Vector2i(xPLUS1, yPLUS1));
-			n[4] = this.getCell(new Vector2i(xPLUS1, yPLUS2));
+			n[0] = this.getCell(new Vector2i(xMIN1, yMIN2));
+			n[1] = this.getCell(new Vector2i(xMIN1, yMIN1));
+			n[2] = this.getCell(new Vector2i(xMIN1, y));
+			n[3] = this.getCell(new Vector2i(xMIN1, yPLUS1));
+			n[4] = this.getCell(new Vector2i(xMIN1, yPLUS2));
 			n[5] = this.getCell(new Vector2i(x, yMIN2));
 			n[6] = this.getCell(new Vector2i(x, yMIN1));
 			n[7] = this.getCell(new Vector2i(x, yPLUS1));
 			n[8] = this.getCell(new Vector2i(x, yPLUS2));
-			n[9] = this.getCell(new Vector2i(xMIN1, yMIN1));
-			n[10] = this.getCell(new Vector2i(xMIN1, y));
-			n[11] = this.getCell(new Vector2i(xMIN1, yPLUS1));
+			n[9] = this.getCell(new Vector2i(xPLUS1, yMIN1));
+			n[10] = this.getCell(new Vector2i(xPLUS1, y));
+			n[11] = this.getCell(new Vector2i(xPLUS1, yPLUS1));
 		}
 		return n;
 	}
@@ -77,7 +79,7 @@ public class TriangleGrid extends Grid
 	{
 		int x = pos.get(0);
 		int y = pos.get(1);
-		if((x % 2) == 0)
+		if((x % 2) != 0)
 		{
 			//Upside-down first triangle
 			if((y % 2) == 0)
