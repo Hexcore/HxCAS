cac5d715f2b0aceeb8530f247d291bbbbd379990
Divan Burger <divan.burger@gmail.com>
2011-07-14 09:57:21 +0200

    Simplify implementation of Cell
    All tests still pass
    
    Change-Id: I1f5b120a83ed74e765202649d3912ea0b4e2955e

--- src/com/hexcore/cas/model/Cell.java
+++ src/com/hexcore/cas/model/Cell.java
@@ -3,52 +3,33 @@ package com.hexcore.cas.model;
 public class Cell
 {
 	private int[] 	values;
-	private int 	valueCount;
 	
 	public Cell(int valueCount)
 	{
 		values = new int[valueCount];
-		this.valueCount = valueCount;
 		
-		for(int i = 0; i < valueCount; i++)
-		{
-			values[i] = 0;
-		}
+		for (int i = 0; i < valueCount; i++) values[i] = 0;
 	}
 	
 	public Cell(int[] values)
 	{
-		valueCount = values.length;
-		this.values = new int[valueCount];
-		                 
-		for(int i = 0; i < valueCount; i++)
-		{
-			this.values[i] = values[i]; 
-		}
+		this.values = (int[])values.clone();
 	}
 	
 	public Cell(Cell cell)
 	{
-		this.valueCount = cell.valueCount;
-		this.values = new int[valueCount];
-		
-		for(int i = 0; i < valueCount; i++)
-		{
-			this.values[i] = cell.values[i];
-		}
+		this.values = (int[])cell.values.clone();
 	}
 	
 	public int getValue(int index)
 	{
-		if(index < 0 || index >= valueCount)
-			return 0;
-		else
-			return values[index];
+		if ((index < 0) || (index >= values.length)) return 0;
+		return values[index];
 	}
 	
 	public int getValueCount()
 	{
-		return valueCount;
+		return values.length;
 	}
 	
 	public int[] getValues()
@@ -58,9 +39,7 @@ public class Cell
 	
 	public void setValue(int index, int value)
 	{
-		if(index < 0 || index >= valueCount)
-			return;
-		else
-			values[index] = value;
+		if ((index < 0) || (index >= values.length)) return;
+		values[index] = value;
 	}
 }
