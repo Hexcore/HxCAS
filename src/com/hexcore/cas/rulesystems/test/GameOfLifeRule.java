1c84845a961005f3660281306c0f4d68a6fe8ba4
Divan Burger <divan.burger@gmail.com>
2011-09-21 19:14:58 +0200

    Add bytecode loader and corresponding tests

---
+++ src/com/hexcore/cas/rulesystems/test/GameOfLifeRule.java
@@ -0,0 +1,23 @@
+package com.hexcore.cas.rulesystems.test;
+
+import com.hexcore.cas.model.Cell;
+import com.hexcore.cas.rulesystems.Rule;
+
+public class GameOfLifeRule implements Rule
+{
+	// Game of Life implementation for testing
+	
+	@Override
+	public void run(Cell cell, Cell[] neighbours)
+	{
+		int sum = 0;
+		
+		for (Cell neighbour : neighbours)
+			sum += neighbour.getValue(0);
+		
+		if(sum < 2 || sum > 3)
+			cell.setValue(0, 0);
+		else if(sum == 3)
+			cell.setValue(0, 1);
+	}
+}
