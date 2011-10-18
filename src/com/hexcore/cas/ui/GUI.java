0dd5f53179d8ee5076b4a137b9fdb6fc253a65e9
Divan Burger <divan.burger@gmail.com>
2011-10-18 13:49:17 +0200

    Separate Viewport

--- src/com/hexcore/cas/ui/GUI.java
+++ src/com/hexcore/cas/ui/GUI.java
@@ -36,7 +36,6 @@ import com.hexcore.cas.rulesystems.CodeGen;
 import com.hexcore.cas.rulesystems.Rule;
 import com.hexcore.cas.rulesystems.RuleLoader;
 import com.hexcore.cas.ui.GUI.RangeContainer;
-import com.hexcore.cas.ui.GUI.Viewport.Type;
 import com.hexcore.cas.ui.toolkit.Button;
 import com.hexcore.cas.ui.toolkit.CheckBox;
 import com.hexcore.cas.ui.toolkit.Colour;
@@ -228,143 +227,6 @@ public class GUI implements WindowEventListener, LobbyListener
 		
 	}
 	
-	
-	public static class Viewport
-	{
-		enum Type {TWO_D, THREE_D};
-		
-		public Grid			grid;
-		public Container	container;
-		public GridWidget	gridWidget;
-		public Type 		type;
-		
-		public ColourRuleSet	colourRuleSet;
-		
-		public Viewport(Container container, Type type, ColourRuleSet colourRuleSet)
-		{
-			this.container = container;
-			this.type = type;
-			this.colourRuleSet = colourRuleSet;
-		}
-
-		public void switchDimension(Grid grid, Window window)
-		{			
-			if (this.type == Viewport.Type.THREE_D)
-				type =  Viewport.Type.TWO_D;
-				
-			else			
-				type =  Viewport.Type.THREE_D;
-			
-			recreate(grid, window);
-		}
-		
-		public void recreate(Grid grid, Window window)
-		{
-			this.grid = grid;
-			
-			Grid3DWidget temp3DWidget = null;
-			Grid2DWidget temp2DWidget = null;
-			
-	    	switch (grid.getType())
-			{
-				case RECTANGLE:
-					if (type == Viewport.Type.THREE_D)
-					{
-						temp3DWidget = new RectangleGrid3DWidget(new Vector2i(10, 10), (RectangleGrid)grid, 10);
-												
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp3DWidget);
-						gridWidget = temp3DWidget;
-					}
-					else
-					{
-						temp2DWidget = new RectangleGridWidget(new Vector2i(10, 10), (RectangleGrid)grid, 10);
-						temp2DWidget.setColourProperty(1);
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp2DWidget);
-						gridWidget = temp2DWidget;
-					}	
-					break;
-				case HEXAGON:
-					if (type == Viewport.Type.THREE_D)
-					{
-						temp3DWidget = new HexagonGrid3DWidget(new Vector2i(10, 10), (HexagonGrid)grid, 10);
-						
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp3DWidget);
-						gridWidget = temp3DWidget;
-					}
-					else
-					{
-						temp2DWidget = new HexagonGridWidget(new Vector2i(10, 10), (HexagonGrid)grid, 10);
-						temp2DWidget.setColourProperty(1);
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp2DWidget);
-						gridWidget = temp2DWidget;
-					}	
-					break;
-				case TRIANGLE:
-					if (type == Viewport.Type.THREE_D)
-					{
-						temp3DWidget = new TriangleGrid3DWidget(new Vector2i(10, 10), (TriangleGrid)grid, 10);
-						
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp3DWidget);
-						gridWidget = temp3DWidget;
-					}
-					else
-					{
-						temp2DWidget = new TriangleGridWidget(new Vector2i(10, 10), (TriangleGrid)grid, 10);
-						temp2DWidget.setColourProperty(1);
-						if (gridWidget != null)
-							if (gridWidget.hasFocus()) 
-								window.requestFocus(temp2DWidget);
-						gridWidget = temp2DWidget;
-					}	
-					break;
-    			
-			}
-	    	
-	    	if (temp3DWidget != null)
-	    	{
-				for (int index = 1; index < 2; index++)
-					temp3DWidget.addSlice(index, 10.0f);
-	    	}
-	    	
-	    	gridWidget.setColourRuleSet(colourRuleSet);
-	    	gridWidget.setFlag(Widget.FILL);
-	    	container.setContents(gridWidget);
-		}
-		
-		void updateControlPanel(LinearLayout controlPanel, Button addSliceButton)
-		{
-			controlPanel.clear();
-			
-			if (this.type == Viewport.Type.THREE_D && grid != null)
-			{
-				Grid3DWidget grid3DWidget = (Grid3DWidget)gridWidget;
-								
-				for (Slice slice : grid3DWidget.getSlices())
-				{
-					DropDownBox colourProperty = new DropDownBox(new Vector2i(100, 20));
-					
-					for (int i = 0; i < grid.getNumProperties(); i++)
-						colourProperty.addItem("Property " + i);
-					
-					colourProperty.setSelected(slice.colourProperty);
-					controlPanel.add(colourProperty);
-				}
-			
-				controlPanel.add(addSliceButton);
-			}
-		}
-	}
-
 	static class ClientEntry implements Comparable<ClientEntry>
 	{
 		InetSocketAddress address;
@@ -1952,7 +1814,7 @@ public class GUI implements WindowEventListener, LobbyListener
             }
             else if (event.target == addSliceButton)
             {
-        		if (selectedViewport != null && selectedViewport.type == Type.THREE_D)
+        		if (selectedViewport != null && selectedViewport.type == Viewport.Type.THREE_D)
         		{
         			Slice slice = new Slice(1, 1.0f);
         			Grid3DWidget gw = (Grid3DWidget)selectedViewport.gridWidget;
