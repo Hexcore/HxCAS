b66ee71b22107fcf6b262d192bd300268aeae21f
Divan Burger <divan.burger@gmail.com>
2011-10-20 15:56:32 +0200

    Fix writing out of colours to only a few decimal places

--- src/com/hexcore/cas/model/ColourRuleSetWriter.java
+++ src/com/hexcore/cas/model/ColourRuleSetWriter.java
@@ -1,5 +1,6 @@
 package com.hexcore.cas.model;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import com.hexcore.cas.ui.toolkit.Colour;
@@ -50,11 +51,13 @@ public class ColourRuleSetWriter
 	
 	private String writeColour(Colour colour)
 	{
+		DecimalFormat formater = new DecimalFormat("#.####");
+		
 		String str = (colour.a < 1.0) ? "rgba(" : "rgb(";
-		str += colour.r;
-		str += ", " + colour.g;
-		str += ", " + colour.b;
-		if (colour.a < 1.0) str += ", " + colour.a;
+		str += formater.format(colour.r);
+		str += ", " + formater.format(colour.g);
+		str += ", " + formater.format(colour.b);
+		if (colour.a < 1.0) str += ", " + formater.format(colour.a);
 		return str + ")";
 	}
 }
