daecad51c0b1cd9990ec9ff6602084a0bd074998
Karl Zöller <karlzoller@yahoo.co.uk>
2013-07-23 22:46:27 +0200

    Added classmexer agent to Server .bat and .sh files.

--- Server.bat
+++ Server.bat
@@ -1,1 +1,1 @@
-java -cp ..;.;jar/asm-3.3.1.jar;jar/cocolibs.jar;jar/gluegen-rt.jar;jar/jogl.all.jar;bin com.hexcore.cas.Server
+java  -javaagent:jar/classmexer.jar -cp ..;.;jar/asm-3.3.1.jar;jar/cocolibs.jar;jar/gluegen-rt.jar;jar/jogl.all.jar;bin com.hexcore.cas.Server
--- Server.sh
+++ Server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ..:.:jar/asm-3.3.1.jar:jar/cocolibs.jar:jar/gluegen-rt.jar:jar/jogl.all.jar:bin com.hexcore.cas.Server
+java -javaagent:jar/classmexer.jar -cp ..:.:jar/asm-3.3.1.jar:jar/cocolibs.jar:jar/gluegen-rt.jar:jar/jogl.all.jar:bin com.hexcore.cas.Server
